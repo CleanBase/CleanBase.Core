3dfe85cb00fb415ddbc7a5bc159be8eb905b2a67
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-16 22:51:13 +0700

    add unit of work

---
+++ CleanBase.Core/Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using CleanBase.Core.Data.Repositories;
+using CleanBase.Core.Data.Transactions;
+using CleanBase.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CleanBase.Core.Data.UnitOfWorks
+{
+	/// <summary>
+	/// Defines the contract for a unit of work, which manages the coordination 
+	/// of transactions and repositories in a data context.
+	/// </summary>
+	public interface IUnitOfWork
+	{
+		/// <summary>
+		/// Begins a new transaction with the specified isolation level.
+		/// </summary>
+		/// <param name="level">The isolation level for the transaction.</param>
+		/// <returns>An instance of <see cref="ICustomTransaction"/> representing the transaction.</returns>
+		ICustomTransaction BeginTransaction(IsolationLevel level);
+
+		/// <summary>
+		/// Begins a new transaction with the default isolation level.
+		/// </summary>
+		/// <returns>An instance of <see cref="ICustomTransaction"/> representing the transaction.</returns>
+		ICustomTransaction BeginTransaction();
+
+		/// <summary>
+		/// Asynchronously saves all changes made in the context to the database.
+		/// </summary>
+		/// <returns>A task representing the asynchronous operation.</returns>
+		Task SaveChangesAsync();
+
+		/// <summary>
+		/// Saves all changes made in the context to the database.
+		/// </summary>
+		void SaveChanges();
+
+		/// <summary>
+		/// Retrieves a repository for the specified entity type.
+		/// </summary>
+		/// <typeparam name="T">The type of the entity.</typeparam>
+		/// <returns>An instance of <see cref="IRepository{T}"/> for the specified entity type.</returns>
+		IRepositoryOfT<T> GetRepositoryByEntityType<T>() where T : class, IEntityCore;
+
+		/// <summary>
+		/// Retrieves a repository of the specified type.
+		/// </summary>
+		/// <typeparam name="TRepo">The type of the repository.</typeparam>
+		/// <returns>An instance of the specified repository type.</returns>
+		TRepo Repository<TRepo>() where TRepo : IRepository;
+	}
+}
---
+++ CleanBase.Core/Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,118 @@
+﻿using CleanBase.Core.Data.Repositories;
+using CleanBase.Core.Data.Transactions;
+using CleanBase.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CleanBase.Core.Data.UnitOfWorks
+{
+	public abstract class UnitOfWork : IUnitOfWork
+	{
+		// Tracks whether the UnitOfWork is disposed.
+		private bool _disposed = false;
+
+		/// <summary>
+		/// Begins a new transaction with the specified isolation level.
+		/// </summary>
+		/// <param name="level">The isolation level for the transaction.</param>
+		/// <returns>An instance of <see cref="ICustomTransaction"/> representing the transaction.</returns>
+		public virtual ICustomTransaction BeginTransaction(IsolationLevel level)
+		{
+			// Create and return a custom transaction with the specified isolation level.
+			return new CustomTransaction(level);
+		}
+
+		/// <summary>
+		/// Begins a new transaction with the default isolation level.
+		/// </summary>
+		/// <returns>An instance of <see cref="ICustomTransaction"/> representing the transaction.</returns>
+		public virtual ICustomTransaction BeginTransaction()
+		{
+			// Begin a transaction with the default isolation level (typically ReadCommitted).
+			return new CustomTransaction(IsolationLevel.ReadCommitted);
+		}
+
+		/// <summary>
+		/// Retrieves a repository of the specified type.
+		/// </summary>
+		/// <typeparam name="TRepo">The type of the repository.</typeparam>
+		/// <returns>An instance of the specified repository type.</returns>
+		public virtual TRepo Repository<TRepo>() where TRepo : IRepository
+		{
+			// Logic to retrieve the specific repository implementation.
+			// This would typically involve resolving it from a service container or a factory.
+			throw new NotImplementedException("Repository retrieval not implemented.");
+		}
+
+		/// <summary>
+		/// Retrieves a repository for the specified entity type.
+		/// </summary>
+		/// <typeparam name="T">The type of the entity.</typeparam>
+		/// <returns>An instance of <see cref="IRepository{T}"/> for the specified entity type.</returns>
+		public virtual IRepositoryOfT<T> GetRepositoryByEntityType<T>() where T : class, IEntityCore
+		{
+			// Logic to retrieve the appropriate repository based on the entity type.
+			throw new NotImplementedException("Repository by entity type retrieval not implemented.");
+		}
+
+		/// <summary>
+		/// Saves all changes made in the context to the database.
+		/// </summary>
+		public virtual void SaveChanges()
+		{
+			// Implement logic to save changes to the database.
+			// Typically, this would involve calling the context's SaveChanges method.
+		}
+
+		/// <summary>
+		/// Asynchronously saves all changes made in the context to the database.
+		/// </summary>
+		/// <returns>A task representing the asynchronous operation.</returns>
+		public virtual async Task SaveChangesAsync()
+		{
+			// Implement logic to save changes to the database asynchronously.
+			// This could be a call to a context's SaveChangesAsync method.
+			await Task.CompletedTask;
+		}
+
+		/// <summary>
+		/// Disposes the UnitOfWork, releasing any resources held by this instance.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Disposes the UnitOfWork, optionally releasing managed resources.
+		/// </summary>
+		/// <param name="disposing">True to release managed resources, otherwise false.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					// Dispose managed resources here, if any.
+					// For example, if the UnitOfWork holds a database context, dispose it here.
+				}
+
+				// Dispose unmanaged resources here, if any.
+				_disposed = true;
+			}
+		}
+
+		/// <summary>
+		/// Destructor to ensure resources are released if Dispose is not called.
+		/// </summary>
+		~UnitOfWork()
+		{
+			Dispose(false);
+		}
+	}
+}
