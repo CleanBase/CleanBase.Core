5f71a6118249182c0f4ab52d22996a9fbc9bad11
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-18 13:23:49 +0700

    fix file naming

---
+++ CleanBase.Core/Helpers/InterpolatedStringBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Helpers
+{
+	public static class InterpolatedStringBuilder
+	{
+		public static string BuildInterpolatedString(int baseLength, int formattedCount, Action<DefaultInterpolatedStringHandler> action)
+		{
+			var handler = new DefaultInterpolatedStringHandler(baseLength, formattedCount);
+			action(handler);
+			return handler.ToStringAndClear();
+		}
+	}
+
+}
