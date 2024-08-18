2afb3003ce77d9422250a578bdd917b34f210207
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-18 15:06:24 +0700

    change string handler method

---
+++ CleanBase.Core/Extensions/InterpolatedStringHandlerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBase.Core.Extensions
+{
+	public static class InterpolatedStringHandlerExtensions
+	{
+		public static string BuildMessage(this DefaultInterpolatedStringHandler handler, string literal, params object[] args)
+		{
+			handler.AppendLiteral(literal);
+			foreach (var arg in args)
+			{
+				handler.AppendFormatted(arg);
+			}
+			return handler.ToStringAndClear();
+		}
+	}
+}
