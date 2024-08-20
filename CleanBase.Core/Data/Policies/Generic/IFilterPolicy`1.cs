5ef423d1d2d2afcfd3c7056cc4849191b2e45cea
hoangvh238 <hoangvh238.dev@gmail.com>
2024-08-20 12:12:06 +0700

    policy update

--- CleanBase.Core/Data/Policies/Generic/IFilterPolicy`1.cs
+++ CleanBase.Core/Data/Policies/Generic/IFilterPolicy`1.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,6 @@ namespace CleanBase.Core.Data.Policies.Generic
 {
 	public interface IFilterPolicy<T> : IDataPolicy
 	{
+		Expression<Func<T, bool>> Predicate();
 	}
 }
