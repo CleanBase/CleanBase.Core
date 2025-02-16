92788b87239e70a0f3bad522560297fb1d0e8c43
hoangvh238 <michael_pham@dealer-fx.com>
2025-02-16 21:59:15 +0700

    Add filter custom attribute

---
+++ CleanBase.Core/CustomAttribute/FilterFieldAttribute.cs
@@ -0,0 +1,20 @@
+﻿using CleanBase.Core.Domain.Exceptions;
+
+namespace CleanBase.Core.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FilterFieldAttribute : Attribute
+    {
+        public string FilterType { get; }
+        public string BindingField { get; }
+
+        public FilterFieldAttribute(string filterType, string bindingField = null)
+        {
+            if (string.IsNullOrWhiteSpace(filterType))
+                throw new DomainException(message: "Filter type cannot be null or empty." + nameof(filterType));
+
+            FilterType = filterType;
+            BindingField = bindingField;
+        }
+    }
+}
